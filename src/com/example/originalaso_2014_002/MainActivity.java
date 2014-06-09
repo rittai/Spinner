63b279416328443fb52598a14caf3eed5b292e5a
student <student@PC111S14.fgakusei.local>
2014-06-09 14:21:02 +0900

    a

--- src/com/example/originalaso_2014_002/MainActivity.java
+++ src/com/example/originalaso_2014_002/MainActivity.java
@@ -8,42 +8,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
+	@Override
+	protected void onResume(){
+	super.onResume();
+	Button btn = (Button)findViewById(R.id.button1);
+    btn.setOnClickListener(this);
+    Button btn2 = (Button)findViewById(R.id.button2);
+    btn2.setOnClickListener(this);
+    Button btn3 = (Button)findViewById(R.id.button3);
+    btn3.setOnClickListener(this);
+	}
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		Button btn = (Button)findViewById(R.id.button3);
-	    btn.setOnClickListener(new View.OnClickListener() {
-	    	
-	    	
-	    	@Override
-	    	public void onClick(View v){
-	    		
-	    		Intent intent = null;
-	    		
-	    		intent = new Intent(MainActivity.this, HitokotoActivity.class);
-				startActivity(intent);
-
-	    	}
-	    });
-		
-	    Button btn2 = (Button)findViewById(R.id.button1);
-	    btn2.setOnClickListener(new View.OnClickListener() {
-		
-	    	@Override
-	    	public void onClick(View v){
-	    		
-	    		Intent intent = null;
-	    		
-	    		intent = new Intent(MainActivity.this, DeleteActivity.class);
-	    		startActivity(intent);
-	    	}
-		
-	    });
 	}
+	
+	@Override
+	public void onClick(View v){
+		Intent intent = null;
+		switch(v.getId()){
+		case R.id.button1:
+			intent = new Intent(this, DeleteActivity.class);
+			startActivity(intent);
+			break;
+			
+		case R.id.button2:
+			break;
+			
+		case R.id.button3:
+			intent = new Intent(this, HitokotoActivity.class);
+			startActivity(intent);
+			break;
+		}
+	}
+	
+		
+		
+
+
 
 	
 
