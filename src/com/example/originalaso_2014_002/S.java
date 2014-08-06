a96188de52bc05bb49d924dfeabf1ce60dc06cae
tokusyou <tokusyou@192.168.1.19>
2014-08-06 11:01:30 +0900

    a

---
+++ src/com/example/originalaso_2014_002/S.java
@@ -0,0 +1,13 @@
+package com.example.originalaso_2014_002;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class S extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.student);
+	}
+}
---
+++ src/com/example/originalaso_2014_002/T.java
@@ -0,0 +1,14 @@
+package com.example.originalaso_2014_002;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class T extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.teacher);
+	}
+
+}
