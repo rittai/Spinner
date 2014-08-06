dc45bf00271a1ff6bbda0a70023c2cb34d6c5f3b
tokusyou <tokusyou@172.31.11.169>
2014-08-06 13:15:19 +0900

    ２

--- src/com/example/originalaso_2014_002/S.java
+++ src/com/example/originalaso_2014_002/S.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class S extends Activity{
-	
+	//生徒画面表示
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
--- src/com/example/originalaso_2014_002/T.java
+++ src/com/example/originalaso_2014_002/T.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class T extends Activity{
-	
+	//教師画面表示
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
